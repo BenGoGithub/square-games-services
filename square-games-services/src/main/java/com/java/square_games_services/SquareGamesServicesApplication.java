59d48255dab10a3d9e79d3bbfbed3a26299cef2f
benjamin.guenoun <benjamin.guenoun@le-campus-numerique.fr>
2024-12-13 14:37:30 +0100

    Départ Square Games Services

---
+++ square-games-services/src/main/java/com/java/square_games_services/SquareGamesServicesApplication.java
@@ -0,0 +1,13 @@
+package com.java.square_games_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareGamesServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquareGamesServicesApplication.class, args);
+	}
+
+}
---
+++ square-games-services/src/test/java/com/java/square_games_services/SquareGamesServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.square_games_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareGamesServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
